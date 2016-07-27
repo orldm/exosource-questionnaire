51ae199ccbfa98eebc8fc8a4fd760c69e1b2e51b
Dmitri <orldm@mail.ru>
2016-07-27 11:54:11 +0300

    add templates

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-    $('.button-start').mousedown(function(e) {
-        $(this).css('top', '0').css('background-color', '#FFFFFF');
+    $('#main-section').load('/assets/templates/page01.html', function(){
+        $('.button-start').mousedown(function(e) {
+            $(this).css('top', '0').css('background-color', '#FFFFFF');
+        });
+        $('.button-start').mouseup(function(e) {
+            $(this).css('top', '-5px').css('background-color', '#EEEEEE');
+            $('#js-progress-bar-container').css('display', 'block');
+            $('#main-section').load('/assets/templates/page02.html');
+        });
     });
-    $('.button-start').mouseup(function(e) {
-        $(this).css('top', '-5px').css('background-color', '#EEEEEE');
-    });
+
+    // var mainSection = $('#page01').html();
+    // console.log(mainSection);
+    
     // $('.js-anchor').click(function(e) {
     //     e.preventDefault();
     //     $('.navbar-collapse').removeClass('in');
