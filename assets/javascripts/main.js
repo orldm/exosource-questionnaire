86b5f674b2e22f38c68da78962a2254eb733653b
Dmitri <orldm@mail.ru>
2016-07-29 12:00:36 +0300

    refactor js and add font

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-    $('#main-section').load('/assets/templates/page01.html', function(){
-        $('.button-start').mousedown(function(e) {
-            $(this).css('top', '0').css('background-color', '#FFFFFF');
-        });
-        $('.button-start').mouseup(function(e) {
-            $(this).css('top', '-5px').css('background-color', '#EEEEEE');
-            $('#js-progress-bar-container').css('display', 'block');
-            $('#main-section').load('/assets/templates/page02.html');
-        });
+    $('.button-start').mousedown(function(e) {
+        $(this).css('top', '0').css('background-color', '#FFFFFF');
     });
-
-    // var mainSection = $('#page01').html();
-    // console.log(mainSection);
-    
-    // $('.js-anchor').click(function(e) {
-    //     e.preventDefault();
-    //     $('.navbar-collapse').removeClass('in');
-    //     $('.navbar-header').removeClass('background-dark')
-    //     $('html, body').animate({
-    //         scrollTop: $( $(this).attr('href') ).offset().top
-    //     }, 500);
-    // });
+    $('.button-start').mouseup(function(e) {
+        $(this).css('top', '-5px').css('background-color', '#EEEEEE');
+        $('#js-progress-bar-container').css('display', 'block');
+        $('#page00').addClass('display-none');
+        $('#page01').removeClass('display-none');
+    });
+    $('.vote-item').click(function(e) {
+        $(this).siblings().find('span').addClass('hidden');
+        $(this).prevAll().find('svg').css('fill', '#98D82A');
+        $(this).find('span').css('color', '#98D82A');
+        $(this).find('svg').css('fill', '#98D82A');
+        var currentPage = '#' + $(this).parents('.main-section').attr('id');
+        var nextPage = '#page0' + (parseInt($(this).parents('.main-section').attr('id').slice(-1), 10) + 1);
+        setTimeout(function() {
+            console.log(currentPage, nextPage);
+            $(currentPage).addClass('display-none');
+            $(nextPage).removeClass('display-none');
+        }, 500);
+    });
 });
